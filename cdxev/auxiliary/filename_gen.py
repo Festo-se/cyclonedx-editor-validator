93e89abf1b1d737262aa12a0627a766e978d28d5
Moritz Marseu <moritz.marseu@festo.com>
2024-01-24 09:01:44 +0100

    Refactor filename generation and validation

---
+++ cdxev/auxiliary/filename_gen.py
@@ -0,0 +1,111 @@
+import typing as t
+from datetime import datetime, timezone
+import logging
+import re
+
+from dateutil.parser import isoparse
+
+
+logger = logging.getLogger(__name__)
+
+
+def generate_filename(sbom: dict) -> str:
+    """
+    Generates a filename for the given SBOM.
+
+    If the SBOM doesn't contain any of the required metadata to generate a unique filename,
+    it will default to ``bom.json``.
+
+    :param dict sbom: The SBOM to generate a filename for.
+
+    :return: The filename.
+    """
+    name = sbom.get("metadata", {}).get("component", {}).get("name")
+    version = sbom.get("metadata", {}).get("component", {}).get("version")
+    timestamp_str: t.Union[str, None] = sbom.get("metadata", {}).get("timestamp")
+
+    if not name and not version and not timestamp_str:
+        return "bom.json"
+
+    try:
+        timestamp = isoparse(timestamp_str)  # type: ignore # because type errors are caught below
+    except (ValueError, TypeError):
+        logger.info(
+            "SBOM has no or an unparsable timestamp. Using current time in filename."
+        )
+        timestamp = datetime.now(timezone.utc)
+
+    name = name or "unknown"
+    timestamp_str = _timestamp_to_utc_str(timestamp)
+
+    components = [name]
+    if version:
+        components.append(version)
+
+    components.append(timestamp_str)
+
+    return "_".join(components) + ".cdx.json"
+
+
+def generate_validation_pattern(sbom: dict) -> str:
+    """
+    Creates a regular expression which can be used to validate the filename of the given SBOM.
+
+    The pattern allows the following variants of the filename:
+
+    * ``bom.json`` is always an allowed name.
+    * ``<name>_[<version>_][<hash>_<timestamp>|<hash>|<timestamp>].cdx.json``, where
+      * ``<name>`` == ``metadata.component.name``, if it exists, otherwise ``unknown``.
+      * ``<version>`` MUST be present, if and only if ``metadata.component.version`` exists.
+      * ``<hash>`` is ``metadata.component.hashes[x].content`` for
+
+    :param dict sbom: The SBOM whose filename to validate.
+
+    :return: A regular expression.
+    """
+    regex = "bom\\.json|"
+
+    name = sbom.get("metadata", {}).get("component", {}).get("name", "unknown")
+    regex += re.escape(name) + "_"
+
+    version = sbom.get("metadata", {}).get("component", {}).get("version")
+    if version:
+        regex += re.escape(version) + "_"
+
+    timestamp = sbom.get("metadata", {}).get("timestamp")
+    if timestamp:
+        try:
+            timestamp_regex = _timestamp_to_utc_str(isoparse(timestamp))
+        except:
+            timestamp_regex = "[0-9]{8}T[0-9]{6}"
+    else:
+        timestamp_regex = "[0-9]{8}T[0-9]{6}"
+
+    hashes = [
+        hash["content"]
+        for hash in sbom.get("metadata", {}).get("component", {}).get("hashes", [])
+    ]
+    hashes = "(" + "|".join(hashes) + ")"
+
+    if not hashes:
+        regex += timestamp_regex
+    else:
+        regex += f"({hashes}|{hashes}_{timestamp_regex}|{timestamp_regex})"
+
+    regex += "\\.cdx\\.json"
+
+    return regex
+
+
+def _timestamp_to_utc_str(timestamp: datetime) -> str:
+    """
+    Converts a timestamp to the string format used in filenames.
+
+    The timestamp will be converted to UTC if it isn't already.
+
+    :param datetime timestamp: The timestamp to convert.
+
+    :return: The string representation for use in the filename.
+    """
+    timestamp = timestamp.astimezone(timezone.utc)
+    return timestamp.strftime("%Y%m%dT%H%M%S")
--- cdxev/validator/helper.py
+++ cdxev/validator/helper.py
@@ -1,6 +1,4 @@
 import json
-import re
-from datetime import datetime
 from importlib import resources
 from pathlib import Path
 
@@ -51,70 +49,6 @@ def load_spdx_schema() -> dict:
     )
     with path_to_embedded_schema.open() as f:
         return json.load(f)
-
-
-def validate_filename(sbom: dict, file: Path, filename_regex: str) -> bool:
-    if filename_regex:
-        valid_filename = re.search(filename_regex, file.name)
-    else:
-        try:
-            iso_timestamp = datetime.fromisoformat(
-                sbom.get("metadata", {}).get("timestamp", "").replace("Z", "+00:00")
-            ).strftime("%Y%m%dT%H%M%S")
-        except ValueError:
-            return False
-        name_of_component = (
-            sbom.get("metadata", {}).get("component", {}).get("name", "")
-        )
-        version_of_component = (
-            sbom.get("metadata", {}).get("component", {}).get("version", "")
-        )
-        hashes_of_component = (
-            sbom.get("metadata", {}).get("component", {}).get("hashes", [])
-        )
-        if not name_of_component or not version_of_component:
-            return False
-
-        component_hash = (
-            "([a-fA-F0-9]{32}|"
-            + "[a-fA-F0-9]{40}|"
-            + "[a-fA-F0-9]{64}|"
-            + "[a-fA-F0-9]{96}|"
-            + "[a-fA-F0-9]{128}"
-        )
-        if len(hashes_of_component) > 0:
-            filename_splitted = file.name.replace(".cdx.json", "").split("_")
-            for hash in hashes_of_component:
-                component_first_hash = [
-                    filename_part
-                    for filename_part in filename_splitted
-                    if hash["content"].startswith(filename_part)
-                ]
-            if component_first_hash:
-                component_hash = "(" + component_first_hash[0]
-            # if hash in file name is not one of the hashes in metadata, file name can not be valid
-            else:
-                return False
-        valid_filename = re.search(
-            "^"
-            + re.escape(name_of_component)
-            + "_"
-            + re.escape(version_of_component)
-            + "_("
-            + component_hash
-            + ")_"
-            + iso_timestamp
-            + ")|"
-            + component_hash
-            + "|"
-            + iso_timestamp
-            + ")(.cdx.json)$|^bom.json$",
-            file.name,
-        )
-    if valid_filename is not None:
-        return True
-    else:
-        return False
 
 
 def get_external_schema(schema_path: Path) -> tuple[dict, Path]:

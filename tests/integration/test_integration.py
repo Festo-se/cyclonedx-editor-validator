a609fa17025dd055be9f65bd91b3440b6772a4b2
Moritz Marseu <moritz.marseu@festo.com>
2024-05-24 16:14:16 +0200

    tests: Add integration tests for general CLI options

--- tests/integration/test_integration.py
+++ tests/integration/test_integration.py
@@ -1,11 +1,115 @@
 import json
+import os
+import re
+from collections.abc import Callable
 from itertools import chain
 from pathlib import Path
 from typing import TypedDict
 
 import pytest
+import toml
 
+from cdxev.__main__ import Status
 from tests.integration.helper import delete_non_reproducible, load_sbom, run_main
+
+
+def test_help(argv: Callable[..., None], capsys: pytest.CaptureFixture[str]):
+    argv("--help")
+    with pytest.raises(SystemExit) as e:
+        run_main()
+
+    assert e.value.code == Status.OK
+    stdout, _ = capsys.readouterr()
+    assert stdout.startswith("usage: ")
+
+
+def test_no_options(capsys: pytest.CaptureFixture[str]):
+    with pytest.raises(SystemExit) as e:
+        run_main()
+
+    assert e.value.code == Status.USAGE_ERROR
+    _, stderr = capsys.readouterr()
+    assert stderr.startswith("usage: ")
+
+
+def test_version(argv: Callable[..., None], capsys: pytest.CaptureFixture[str]):
+    argv("--version")
+    with pytest.raises(SystemExit) as e:
+        run_main()
+
+    assert e.value.code == Status.OK
+    stdout, _ = capsys.readouterr()
+    assert re.match(r"[0-9]+\.[0-9]+\.[0-9]+", stdout) is not None
+
+
+@pytest.mark.skipif(
+    "CI" not in os.environ, reason="Run only in CI, after a clean install"
+)
+def test_version_from_pyproject(
+    argv: Callable[..., None], capsys: pytest.CaptureFixture[str]
+):
+    toml_file = Path(__file__).parents[2] / "pyproject.toml"
+    toml_content = toml.load(toml_file)
+    expected_version = toml_content.get("tool", {}).get("poetry", {}).get("version")
+
+    argv("--version")
+    with pytest.raises(SystemExit) as e:
+        run_main()
+
+    assert e.value.code == Status.OK
+    stdout, _ = capsys.readouterr()
+    assert stdout.strip() == expected_version
+
+
+def test_dir_output(
+    argv: Callable[..., None],
+    data_dir: Path,
+    tmp_path: Path,
+    capsys: pytest.CaptureFixture[str],
+):
+    # The actual command and inputs don't matter for this test.
+    # It is only meant to test that output to a directory works.
+    argv(
+        "amend",
+        "--output",
+        str(tmp_path),
+        str(data_dir / "amend.input.cdx.json"),
+    )
+    exit_code, output_file, _ = run_main(capsys, "filename")
+
+    # Verify that command completed successfully
+    assert exit_code == Status.OK
+
+    # Verify that output matches what is expected
+    actual = load_sbom(tmp_path / output_file)
+    expected = load_sbom(data_dir / "amend.expected_default.cdx.json")
+    assert actual == expected
+
+
+def test_file_output(
+    argv: Callable[..., None],
+    data_dir: Path,
+    tmp_path: Path,
+    capsys: pytest.CaptureFixture[str],
+):
+    # The actual command and inputs don't matter for this test.
+    # It is only meant to test that output to a file works.
+    output_file = tmp_path / "output.cdx.json"
+    argv(
+        "amend",
+        "--output",
+        str(output_file),
+        str(data_dir / "amend.input.cdx.json"),
+    )
+    exit_code, *_ = run_main(capsys)
+
+    # Verify that command completed successfully
+    assert exit_code == Status.OK
+
+    # Verify that output matches what is expected
+    actual = load_sbom(output_file)
+    expected = load_sbom(data_dir / "amend.expected_default.cdx.json")
+    assert actual == expected
 
 
 class TestAmend:

b642b5f73c11d488c566c647bc38780474fe4c93
chbk <christian.beck@festo.com>
2024-06-03 16:08:29 +0200

    chore: black

--- cdxev/auxiliary/sbomFunctions.py
+++ cdxev/auxiliary/sbomFunctions.py
@@ -187,8 +187,7 @@ def get_bom_refs_from_dependencies(dependencies: Sequence[dict]) -> list[str]:
 
 
 def get_ref_from_components(
-        list_of_components: Sequence[dict],
-        only_top_level: bool = True
+    list_of_components: Sequence[dict], only_top_level: bool = True
 ) -> list[str]:
     """
     Function that returns a list of bom-refs from a list of components.

c6d0f4fa804f235188807cb5fbcf8774f0310524
Moritz Marseu <moritz.marseu@festo.com>
2024-06-17 16:57:07 +0200

    chore: Add GPL-3.0 license header comment
    Fixes #194

--- cdxev/build_public_bom.py
+++ cdxev/build_public_bom.py
@@ -1,8 +1,18 @@
-#################################################
-# Function to remove internal components based on
-# a provided JSON schema, that contains the
-# requirements for a component to be considered internal.
-#################################################
+# CycloneDX Editor Validator
+# Copyright (C) 2023  Festo SE & Co. KG
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <https://www.gnu.org/licenses/>.
 
 import json
 import re
--- tests/auxiliary/sbomFunctionsTests.py
+++ tests/auxiliary/sbomFunctionsTests.py
@@ -1,6 +1,18 @@
-##################################################
-# Module with functions for the work with sboms
-###################################################
+# CycloneDX Editor Validator
+# Copyright (C) 2023  Festo SE & Co. KG
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <https://www.gnu.org/licenses/>.
 
 
 def compare_sboms(first_sbom: dict, second_sbom: dict) -> bool:

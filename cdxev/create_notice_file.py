255665b60e286b3c2325ef1fdd3f89758f8c1c04
chbk <christian.beck@festo.com>
2024-06-26 22:37:16 +0200

    tests: includes sbom for tests

--- cdxev/create_notice_file.py
+++ cdxev/create_notice_file.py
@@ -1,7 +1,7 @@
 from cdxev.auxiliary.sbomFunctions import extract_components
 
 
-def print_license(license: dict) -> str:
+def extract_license(license: dict) -> str:
     if license.get("expression", ""):
         return license.get("expression", "")
     elif license.get("license", {}).get("id", ""):
@@ -28,7 +28,7 @@ def create_notice_file(sbom: dict) -> str:
         header += product_copyright + "\n"
     elif product_licenses:
         for license in product_licenses:
-            header += print_license(license) + ","
+            header += extract_license(license) + ","
         header.rstrip(",")
 
     text_body = ""
@@ -41,11 +41,11 @@ def create_notice_file(sbom: dict) -> str:
         if component.get("copyright", ""):
             component_copyright_information = component.get("copyright", "")
         for license in component.get("licenses", []):
-            if print_license(license):
-                component_license_information += print_license(license) + "\n"
+            if extract_license(license):
+                component_license_information += extract_license(license) + "\n"
 
         if not component_copyright_information and not component_license_information:
-            component_information += "No license or copyright information available"
+            component_information += "No license or copyright information available \n"
 
         elif component_copyright_information and not component_license_information:
             component_information += component_copyright_information + "\n"
@@ -55,10 +55,11 @@ def create_notice_file(sbom: dict) -> str:
 
         else:
             component_information += component_copyright_information + "\n"
-            component_information += component_license_information
+            component_information += component_license_information + "\n"
 
         text_body += component_information
 
+    text_body = text_body.removesuffix('\n')
     notice_file = header
     if text_body:
         notice_file += (

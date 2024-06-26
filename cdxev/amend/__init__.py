fb8e300f6e5ee4e9ccaeec3210cc06a2560a656a
Moritz Marseu <moritz.marseu@festo.com>
2024-06-26 10:02:31 +0200

    Add copyright to header

--- cdxev/amend/__init__.py
+++ cdxev/amend/__init__.py
@@ -1,1 +1,2 @@
+# Copyright: 2023 - 2024 Festo SE & Co. KG
 # SPDX-License-Identifier: GPL-3.0-or-later
--- cdxev/error.py
+++ cdxev/error.py
@@ -1,3 +1,4 @@
+# Copyright: 2023 - 2024 Festo SE & Co. KG
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 from typing import Optional

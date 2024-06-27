c29ada9d13b4d94e7d39ef4364a1c01e77e8be8a
Moritz Marseu <moritz.marseu@festo.com>
2024-06-27 09:20:49 +0200

    chore: remove copyright statement

--- cdxev/amend/__init__.py
+++ cdxev/amend/__init__.py
@@ -1,2 +1,1 @@
-# Copyright: 2023 - 2024 Festo SE & Co. KG
 # SPDX-License-Identifier: GPL-3.0-or-later
--- tests/test_error.py
+++ tests/test_error.py
@@ -1,4 +1,3 @@
-# Copyright: 2023 - 2024 Festo SE & Co. KG
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 import unittest

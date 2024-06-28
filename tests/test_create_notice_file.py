5fa4f3846f2352afa0717cbb417d8cbeb204a9ae
chbk <christian.beck@festo.com>
2024-06-28 22:45:45 +0200

    tests: adds tests for creation of notice file

--- tests/test_create_notice_file.py
+++ tests/test_create_notice_file.py
@@ -15,8 +15,58 @@ def get_test_sbom(pathsbom: str = path_to_sbom) -> dict:
 
 
 class TestCreateNoticeFile(unittest.TestCase):
-
     def test_extract_license(self) -> None:
+        expression = "Example, Inc. Commercial License"
+        id = "Apache-1.0"
+        name = "Apache 1.0"
+        license_expression = {"expression": expression}
+        license_id = {"license": {"id": id}}
+        license_name = {"license": {"name": name}}
+        extracted_expression = NoticeFile.extract_license(license_expression)
+        extracted_id = NoticeFile.extract_license(license_id)
+        extracted_name = NoticeFile.extract_license(license_name)
+        extracted_empty_license = NoticeFile.extract_license({})
+
+        self.assertEqual(extracted_expression, expression)
+        self.assertEqual(extracted_id, id)
+        self.assertEqual(extracted_name, name)
+        self.assertEqual(extracted_empty_license, "")
+
+    def test_create_notice_file(self) -> None:
         sbom = get_test_sbom()
-        print(NoticeFile.create_notice_file(sbom))
-        
+        notice_file = NoticeFile.create_notice_file(sbom)
+        components = NoticeFile.extract_components(sbom.get("components", []))
+        notice_file_licenses = notice_file[
+            notice_file.find(
+                "This product includes material developed by third parties: \n"
+            )
+            + len("This product includes material developed by third parties: \n")
+            + 1 :
+        ]
+        notice_file_licenses_split = notice_file_licenses.split("\n\n")
+
+        for component in components:
+            found = False
+            for license_entry in notice_file_licenses_split:
+                license_component_name = license_entry[: license_entry.find(":\n")]
+                license_component_name = license_component_name.replace("\n", "")
+                licenses = license_entry[license_entry.find(":\n") + len(":\n") :]
+                license_list = licenses.split("\n")
+                license_list = [entry.replace("\n", "") for entry in license_list]
+
+                if component.get("name", "") != license_component_name:
+                    continue
+
+                if (
+                    component.get("copyright", "")
+                    and component.get("copyright", "") not in license_list
+                ):
+                    continue
+                for license in component.get("licenses", []):
+                    license_content = NoticeFile.extract_license(license)
+                    if license_content not in license_list:
+                        continue
+
+                found = True
+
+            self.assertTrue(found)

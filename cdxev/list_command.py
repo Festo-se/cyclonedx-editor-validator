8fc82f70fd4d999c6bffb1b9c1c15a84ac13fd26
chbk <christian.beck@festo.com>
2024-10-17 04:14:25 +0200

    feat: implements list_command functions for licenses and components

--- cdxev/list_command.py
+++ cdxev/list_command.py
@@ -1,6 +1,6 @@
 import json
 import logging
-from cdxev.auxiliary.sbomFunctions import deserialize
+from cdxev.auxiliary.sbomFunctions import deserialize, extract_cyclonedx_components
 from cdxev.error import AppError
 from cdxev.log import LogMessage
 
@@ -9,7 +9,9 @@ from cyclonedx.model.license import (
     LicenseExpression,
     DisjunctiveLicense,
 )
-from cyclonedx.model.bom import Bom
+from cyclonedx.model.bom import Bom, BomMetaData
+from cyclonedx.model.component import Component
+from typing import Any
 
 
 logger = logging.getLogger(__name__)
@@ -42,44 +44,285 @@ def extract_string_from_license(license: License) -> str:
         return ""
 
 
-def list_license_information(sbom: dict) -> str:
-    deserialized_bom = deserialize(sbom)
-    metadata = deserialized_bom.metadata
+def extract_license_strings_from_licenses(licenses: list[License]) -> list[str]:
+    license_list = []
+    for license in licenses:
+        license_string = extract_string_from_license(license)
+        if license_string:
+            license_list.append(license_string)
+    return license_list
 
-    product_licenses = [License]
-    product_copyright = ""
 
-    header = [str]
-    text_body = [str]
+def extract_metadata_license_information(metadata: BomMetaData) -> dict:
     if metadata.component is not None:
         metadata_component = metadata.component
-
+        software_information: dict[str, Any] = {}
         if metadata_component.name is not None:
-            header.append(metadata_component.name)
-            header.append("\n")
-        else:
-            logger.info(
-                LogMessage(
-                    "SBOM has no metadata.component",
-                )
-            )
+            software_information["name"] = metadata_component.name
 
         if metadata_component.licenses is not None:
-            product_licenses = metadata_component.licenses
+            license_list = extract_license_strings_from_licenses(
+                metadata_component.licenses
+            )
+            software_information["licenses"] = license_list
+
         if metadata_component.copyright is not None:
-            product_copyright = metadata_component.copyright
+            software_information["copyright"] = metadata_component.copyright
 
-    if not product_licenses and metadata.licenses is not None:
-        product_licenses = metadata.licenses
+    else:
+        logger.info(
+            LogMessage(
+                message="SBOM has no metadata.component",
+                description=(
+                    "The SBOM has no metadata so no information "
+                    "about the software could be extracted"
+                ),
+            )
+        )
+
+    return software_information
 
 
-    if product_copyright:
-        header.append(product_copyright)
-    elif product_licenses:
-        for license in product_licenses:
-            license_string = extract_string_from_license(license)
-            if license_string:
-                header.append(license_string)
+def extract_components_metadata_information(
+    components: list[Component],
+) -> list[dict[str, Any]]:
+    extracted_components = extract_cyclonedx_components(components)
+
+    list_of_license_information: list[dict[str, Any]] = []
+
+    for component in extracted_components:
+        license_information: dict[str, Any] = {}
+        not_empty = False
+        if component.name is not None:
+            license_information["name"] = component.name
+
+        if component.licenses is not None:
+            license_list = extract_license_strings_from_licenses(component.licenses)
+            license_information["licenses"] = license_list
+            not_empty = True
+        if component.copyright is not None:
+            license_information["copyright"] = component.copyright
+            not_empty = True
+
+        if not_empty:
+            list_of_license_information.append(license_information)
+
+    return list_of_license_information
 
 
-    return product_name
+def write_list_to_str(str_list: list[str], division_character: str = "\n") -> str:
+    string = ""
+    if str_list:
+        string += str_list[0]
+    for index in range(1, len(str_list)):
+        string += division_character
+        string += str_list[index]
+    return string
+
+
+def write_license_dict_to_txt(info_dict: dict) -> str:
+    string = ""
+
+    if info_dict.get("name", ""):
+        string += info_dict.get("name", "") + ":"
+
+    if info_dict.get("copyright", ""):
+        string += "\n"
+        string += info_dict.get("copyright", "")
+
+    if info_dict.get("licenses", ""):
+        license_str = write_list_to_str(info_dict.get("licenses", ""))
+        string += "\n"
+        string += license_str
+
+    if not info_dict.get("licenses", "") and not info_dict.get("copyright", ""):
+        string += "\n"
+        string += "No license or copyright information available"
+
+    return string
+
+
+def write_license_dict_to_csv(info_dict: dict) -> str:
+    string = ""
+
+    if info_dict.get("name", ""):
+        string += info_dict.get("name", "")
+
+    if info_dict.get("copyright", ""):
+        string += ","
+        string += info_dict.get("copyright", "")
+
+    if info_dict.get("licenses", ""):
+        license_str = write_list_to_str(info_dict.get("licenses", ""), ",")
+        string += ","
+        string += license_str
+
+    return string
+
+
+def write_license_information_to_txt(
+    software_information: dict,
+    component_information: list[dict],
+    list_metadata: bool = True,
+) -> str:
+
+    if list_metadata:
+        string = write_license_dict_to_txt(software_information)
+
+        if not component_information:
+            return string
+
+        string += "\n\n"
+        string += "This product includes material developed by third parties:"
+        string += "\n\n"
+
+    else:
+        string = ""
+
+    for entry in component_information:
+        if entry.get("name", ""):
+            string += write_license_dict_to_txt(entry)
+            string += "\n\n"
+
+    string.rstrip("\n\n")
+
+    return string
+
+
+def write_license_information_to_csv(
+    software_information: dict,
+    component_information: list[dict],
+    list_metadata: bool = True,
+) -> str:
+    string = "Name,Copyright,Licenses"
+
+    if list_metadata:
+        string += "\n" + write_license_dict_to_csv(software_information)
+
+    if not component_information:
+        return string
+
+    for entry in component_information:
+        if entry.get("name", ""):
+            string += "\n"
+            string += write_license_dict_to_csv(entry)
+
+    return string
+
+
+def list_license_information(
+    sbom: Bom, format: str = "txt", list_metadata: bool = True
+) -> str:
+
+    metadata = sbom.metadata
+
+    software_information = extract_metadata_license_information(metadata)
+    component_information = extract_components_metadata_information(sbom.components)
+    if format == "txt":
+        txt_string = write_license_information_to_txt(
+            software_information, component_information, list_metadata=list_metadata
+        )
+
+    if format == "csv":
+        txt_string = write_license_information_to_csv(
+            software_information, component_information, list_metadata=list_metadata
+        )
+
+    return txt_string
+
+
+def list_component_information(
+    component: Component, division_character: str = "\n"
+) -> str:
+    string = ""
+    if component.name is not None:
+        string += component.name
+    else:
+        return ""
+
+    if component.version is not None:
+        string += division_character
+        string += component.version
+
+    if component.supplier is not None:
+        if component.supplier.name is not None:
+            string += division_character
+            string += component.supplier.name
+
+    return string
+
+
+def list_components(sbom: Bom, list_metadata: bool = True, format: str = "txt") -> str:
+    if format == "txt":
+        division_character = "\n"
+        string = ""
+        line_break = "\n\n"
+    elif format == "csv":
+        division_character = ","
+        string = "Name,Version,Supplier Name"
+        line_break = "\n"
+
+    if list_metadata:
+        if sbom.metadata.component is not None:
+            string += "\n"
+            string += list_component_information(
+                sbom.metadata.component, division_character
+            )
+
+    if sbom.components is not None:
+        components = extract_cyclonedx_components(sbom.components)
+        for component in components:
+            string += line_break
+            string += list_component_information(component, division_character)
+
+    return string
+
+
+def list_command(
+    sbom: dict, operation: str, format: str = "txt", list_metadata: bool = True
+) -> None:
+    deserialized_bom = deserialize(sbom)
+
+    if operation == "list-licenses":
+        license_list = list_license_information(
+            sbom=deserialized_bom, format=format, list_metadata=list_metadata
+        )
+        print(license_list)
+    elif operation == "list-components":
+        components_list = list_components(
+            sbom=deserialized_bom, list_metadata=list_metadata, format=format
+        )
+        print(components_list)
+
+
+# "Acme_Application_9.1.1_ec7781220ec7781220ec778122012345_20220217T101458.cdx.json",
+with open(
+    "C:/Workspace/Github/cyclonedx-editor-validator/tests/auxiliary/test_create_notice_file_sboms/Acme_Application_9.1.1_20220217T101458.cdx.json",
+    "r",
+) as file:
+    data = json.load(file)
+# data["components"] = data["components"][4]
+# data["components"].pop(4)
+# data["components"].pop(4)
+# data["components"].pop(4)
+# data["components"].pop(4)
+# list_license_information(data)
+list_command(data, "list-components", format="csv")
+print("")
+list_command(data, "list-components", format="csv", list_metadata=False)
+print("")
+print("")
+list_command(data, "list-licenses", format="csv")
+print("")
+list_command(data, "list-licenses", format="csv", list_metadata=False)
+print("")
+print("txt")
+print("")
+list_command(data, "list-components", format="txt")
+print("")
+list_command(data, "list-components", format="txt", list_metadata=False)
+print("")
+print("")
+list_command(data, "list-licenses", format="txt")
+print("txt")
+list_command(data, "list-licenses", format="txt", list_metadata=False)
---
+++ tests/test_list_command.py
@@ -0,0 +1,86 @@
+import json
+import unittest
+
+import cdxev.list_command as lc
+from cdxev.auxiliary.sbomFunctions import deserialize
+
+path_to_sbom = (
+    "tests/auxiliary/test_create_notice_file_sboms/"
+    "Acme_Application_9.1.1_20220217T101458.cdx.json"
+)
+
+
+def get_test_sbom(path_sbom: str = path_to_sbom) -> dict:
+    with open(path_sbom, "r") as read_file:
+        sbom = json.load(read_file)
+    return sbom
+
+
+def extract_license(license: dict) -> str:
+    if license.get("expression", ""):
+        return license.get("expression", "")
+    elif license.get("license", {}).get("id", ""):
+        return license.get("license", {}).get("id", "")
+    else:
+        return license.get("license", {}).get("name", "")
+
+
+class TestCreatenoticefile(unittest.TestCase):
+    """
+    def test_extract_license(self) -> None:
+        expression = "Example, Inc. Commercial License"
+        id = "Apache-1.0"
+        name = "Apache 1.0"
+        license_expression = {"expression": expression}
+        license_id = {"license": {"id": id}}
+        license_name = {"license": {"name": name}}
+        extracted_expression = noticefile.extract_license(license_expression)
+        extracted_id = noticefile.extract_license(license_id)
+        extracted_name = noticefile.extract_license(license_name)
+        extracted_empty_license = noticefile.extract_license({})
+
+        self.assertEqual(extracted_expression, expression)
+        self.assertEqual(extracted_id, id)
+        self.assertEqual(extracted_name, name)
+        self.assertEqual(extracted_empty_license, "")
+    """
+    def test_create_notice_file(self) -> None:
+        sbom = get_test_sbom()
+        deserialized_sbom = deserialize(sbom)
+        license_file = lc.list_license_information(deserialized_sbom)
+        components = (sbom.get("components", []))
+        notice_file_licenses = license_file[
+            license_file.find(
+                "This product includes material developed by third parties:\n\n"
+            )
+            + len("This product includes material developed by third parties:\n")
+            + 1 :
+        ]
+        notice_file_licenses_split = notice_file_licenses.split("\n\n")
+
+        print(notice_file_licenses_split)
+        for component in components:
+            found = False
+            for license_entry in notice_file_licenses_split:
+                license_component_name = license_entry[: license_entry.find(":\n")]
+                license_component_name = license_component_name.replace("\n", "")
+                licenses = license_entry[license_entry.find(":\n") + len(":\n") :]
+                license_list = licenses.split("\n")
+                license_list = [entry.replace("\n", "") for entry in license_list]
+
+                if component.get("name", "") != license_component_name:
+                    continue
+
+                if (
+                    component.get("copyright", "")
+                    and component.get("copyright", "") not in license_list
+                ):
+                    continue
+                for license in component.get("licenses", []):
+                    license_content = extract_license(license)
+                    if license_content not in license_list:
+                        continue
+
+                found = True
+
+            self.assertTrue(found)

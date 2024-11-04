d546945ff27e571e276ea5ef473fbc2bdb8b43be
Aleg Vilinski <58322186+italvi@users.noreply.github.com>
2024-11-04 07:13:44 +0100

    refactor: remove not required warnings filter after update of cyclonedx-python-lib

--- cdxev/initialize_sbom.py
+++ cdxev/initialize_sbom.py
@@ -95,20 +95,16 @@ def initialize_sbom(
         component=metadata_component,
         timestamp=timestamp,
     )
-    with warnings.catch_warnings():
-        warnings.simplefilter("ignore")
-        # ignore warning caused by absence of components
-        # required due to an implementation in the cyclonedx-python-lib
-        #  https://github.com/CycloneDX/cyclonedx-python-lib/issues/617
-        sbom = Bom(
-            version=1,
-            metadata=metadata,
-            dependencies=[Dependency(bom_ref, dependencies=[])],
-        )
 
-        my_json_outputter = JsonV1Dot6(sbom)
-        serialized_json: dict[str, Any] = json.loads(
-            my_json_outputter.output_as_string(indent=4)
-        )
+    sbom = Bom(
+        version=1,
+        metadata=metadata,
+        dependencies=[Dependency(bom_ref, dependencies=[])],
+    )
+
+    my_json_outputter = JsonV1Dot6(sbom)
+    serialized_json: dict[str, Any] = json.loads(
+        my_json_outputter.output_as_string(indent=4)
+    )
 
     return serialized_json

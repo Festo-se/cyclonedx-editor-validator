9803043b1eea4bb2648ff6a3219e85d1aebf97a0
matze08 <matthias.fauser12@gmail.com>
2025-01-28 15:03:13 +0100

    apply: black

--- cdxev/vex.py
+++ cdxev/vex.py
@@ -46,7 +46,9 @@ def get_list_of_ids(input_file: dict[str, Any], scheme: str) -> str:
         list_str += "ID,RefID,Description,Status\n"
         for vulnerability in input_file.get("vulnerabilities", []):
             vul_id = vulnerability.get("id", "-")
-            vul_ref_id = vulnerability.get("references", [{"id": "-"}])[0].get("id", "-")
+            vul_ref_id = vulnerability.get("references", [{"id": "-"}])[0].get(
+                "id", "-"
+            )
             vul_description = vulnerability.get("description", "-")
             vul_state = vulnerability.get("analysis", {}).get("state", "-")
             list_str += (
